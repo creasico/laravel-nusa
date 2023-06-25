4e303b8f08928a0fbeaff14880eddcf055bd6971
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-06-26 05:32:42 +0700

    chore(db): add `latitude`, `longitude` and `coordinates` fields
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>
    
    chore(db): update database import procedures and output files structures
    
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- scripts/Database.php
+++ scripts/Database.php
@@ -2,6 +2,7 @@
 
 namespace Creasi\Scripts;
 
+use Illuminate\Support\Collection;
 use PDO;
 use PDOStatement;
 
@@ -9,8 +10,12 @@ class Database
 {
     private readonly PDO $conn;
 
+    private readonly string $libPath;
+
     public function __construct(string $name, string $host, string $user, ?string $pass = null)
     {
+        $this->libPath = \realpath(\dirname(__DIR__.'/../..'));
+
         $this->conn = new PDO("mysql:dbname={$name};host={$host}", $user, $pass, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         ]);
@@ -28,27 +33,98 @@ class Database
      */
     public static function import($event): void
     {
-        $libPath = \realpath(\dirname(__DIR__.'/../..'));
-
         require_once $event->getComposer()->getConfig()->get('vendor-dir').'/autoload.php';
 
-        $db = new static(
-            name: env('DB_NAME', 'cahyadsn_wilayah'),
+        $self = new static(
+            name: env('DB_NAME', 'nusantara'),
             host: env('DB_HOST', '127.0.0.1'),
             user: env('DB_USER', 'root'),
             pass: env('DB_PASS', 'secret'),
         );
 
-        if ($wilayahSql = file_get_contents($libPath.'/submodules/cahyadsn-wilayah/db/wilayah.sql')) {
-            $db->query($wilayahSql);
+        if ($wilayahSql = file_get_contents($self->libPath.'/submodules/cahyadsn-wilayah/db/wilayah.sql')) {
+            $self->query($wilayahSql);
         }
 
-        if ($pulauSql = file_get_contents($libPath.'/submodules/cahyadsn-wilayah/db/pulau_2022.sql')) {
-            $db->query($pulauSql);
+        if ($coordinatesSql = file_get_contents($self->libPath.'/submodules/cahyadsn-wilayah/db/wilayah_level_1_2.sql')) {
+            $self->query($coordinatesSql);
         }
 
-        if ($coordinatesSql = file_get_contents($libPath.'/submodules/cahyadsn-wilayah/db/wilayah_level_1_2.sql')) {
-            $db->query($coordinatesSql);
+        $stmt = $self->query(<<<SQL
+            SELECT
+                w.kode, w.nama,
+                l.lat, l.lng, l.elv, l.tz, l.luas, l.penduduk, l.path
+            FROM wilayah w
+            LEFT JOIN wilayah_level_1_2 l ON w.kode = l.kode
+        SQL, PDO::FETCH_OBJ);
+
+        $data = collect($stmt->fetchAll())->reduce(function ($regions, $item) {
+            $data = new Normalizer($item->kode, $item->nama, $item->lat, $item->lng, $item->path);
+
+            $regions[$data->type][] = $data->normalize();
+
+            return $regions;
+        }, []);
+
+        foreach ($data as $table => $content) {
+            $content = \collect($content);
+
+            if ($table === 'provinces') {
+                $self->writeCsv($table, $content);
+
+                $self->writeJson($table, $content);
+
+                continue;
+            }
+
+            $content->groupBy('province_code')->each(function (Collection $content, $key) use ($table, $self) {
+                $self->writeCsv($key.'/'.$table, $content);
+
+                $self->writeJson($key.'/'.$table, $content);
+            });
+        }
+    }
+
+    private function writeCsv(string $filename, Collection $content): void
+    {
+        $this->ensureDirectoryExists(
+            $path = "{$this->libPath}/resources/csv/$filename.csv"
+        );
+
+        $csv = [
+            array_keys($content[0]),
+        ];
+
+        foreach ($content as $value) {
+            $value['coordinates'] = \json_encode($value['coordinates']);
+
+            $csv[] = array_values($value);
+        }
+
+        $fp = fopen($path, 'w');
+
+        foreach ($csv as $line) {
+            fputcsv($fp, $line);
+        }
+
+        fclose($fp);
+    }
+
+    private function writeJson(string $filename, Collection $content): void
+    {
+        $this->ensureDirectoryExists(
+            $path = "{$this->libPath}/resources/json/$filename.json"
+        );
+
+        file_put_contents($path, json_encode($content->toArray(), JSON_PRETTY_PRINT));
+    }
+
+    private function ensureDirectoryExists(string $path)
+    {
+        $dir = \dirname($path);
+
+        if (! is_dir($dir)) {
+            \mkdir($dir, 0755);
         }
     }
 }
---
+++ scripts/Normalizer.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Creasi\Scripts;
+
+class Normalizer
+{
+    public readonly string $type;
+
+    public readonly ?array $coordinates;
+
+    public function __construct(
+        public readonly string $code,
+        public readonly string $name,
+        public readonly ?float $latitude = null,
+        public readonly ?float $longitude = null,
+        ?string $coordinates = null,
+    ) {
+        $this->type = match (strlen($this->code)) {
+            2 => 'provinces',
+            5 => 'regencies',
+            8 => 'districts',
+            default => 'villages',
+        };
+
+        $this->coordinates = $coordinates ? \json_decode($coordinates) : null;
+    }
+
+    public function normalize()
+    {
+        return match ($this->type) {
+            'villages' => $this->toVillage(),
+            'districts' => $this->toDistrict(),
+            'regencies' => $this->toRegency(),
+            'provinces' => [
+                'code' => (int) $this->code,
+                'name' => str($this->name)->title(),
+                'latitude' => $this->latitude,
+                'longitude' => $this->longitude,
+                'coordinates' => $this->coordinates,
+            ],
+        };
+    }
+
+    private function toRegency(): array
+    {
+        [$province_code, $code] = explode('.', $this->code, 2);
+
+        return [
+            'code' => (int) ($province_code.$code),
+            'province_code' => (int) $province_code,
+            'name' => str($this->name)->title(),
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'coordinates' => $this->coordinates,
+        ];
+    }
+
+    private function toDistrict(): array
+    {
+        [$province_code, $regency_code, $code] = explode('.', $this->code, 3);
+
+        return [
+            'code' => (int) ($province_code.$regency_code.$code),
+            'regency_code' => (int) ($province_code.$regency_code),
+            'province_code' => (int) $province_code,
+            'name' => $this->name,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'coordinates' => $this->coordinates,
+        ];
+    }
+
+    private function toVillage(): array
+    {
+        [$province_code, $regency_code, $district_code, $code] = explode('.', $this->code, 4);
+
+        return [
+            'code' => (int) ($province_code.$regency_code.$district_code.$code),
+            'district_code' => (int) ($province_code.$regency_code.$district_code),
+            'regency_code' => (int) ($province_code.$regency_code),
+            'province_code' => (int) $province_code,
+            'name' => $this->name,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'coordinates' => $this->coordinates,
+        ];
+    }
+}
