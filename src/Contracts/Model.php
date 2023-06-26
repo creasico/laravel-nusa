edbfd065de64bf849120e7c493340ab3eef65cf1
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-06-26 09:15:27 +0700

    feat: define model contract for easy extendability
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ src/Contracts/Model.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Creasi\Nusa\Contracts;
+
+/**
+ * @property-read int $code
+ * @property-read string $name
+ * @property-read ?float $latitude
+ * @property-read ?float $longitude
+ * @property-read ?array $coordinates
+ */
+interface Model
+{
+    //
+}
---
+++ src/Contracts/Province.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Creasi\Nusa\Contracts;
+
+/**
+ * @property-read \Illuminate\Support\Collection<int, Regency> $regencies
+ * @property-read \Illuminate\Support\Collection<int, District> $districts
+ * @property-read \Illuminate\Support\Collection<int, Village> $villages
+ */
+interface Province extends Model
+{
+    //
+}
