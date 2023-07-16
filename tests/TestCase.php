534b903cb325abbabf508ad6f79ba17e5ac67626
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-07-16 23:32:59 +0700

    fix(tests): fix creasico/laravel-package#47
    and see creasico/laravel-package#48 for the guide
    
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -30,21 +30,31 @@ class TestCase extends Orchestra
             $config->set('app.locale', 'id');
             $config->set('app.faker_locale', 'id_ID');
 
-            if (env('DB_CONNECTION', 'sqlite')) {
-                $config->set('database.default', 'sqlite');
+            $conn = env('DB_CONNECTION', 'sqlite');
 
-                $database = __DIR__.'/test.sqlite';
+            $config->set('database.default', $conn);
 
-                if (! file_exists($database)) {
+            if ($conn === 'sqlite') {
+                if (! file_exists($database = __DIR__.'/test.sqlite')) {
                     touch($database);
                 }
 
-                $config->set('database.connections.sqlite', [
-                    'driver' => 'sqlite',
+                $this->mergeConfig($config, 'database.connections.sqlite', [
                     'database' => $database,
                     'foreign_key_constraints' => true,
                 ]);
+            } else {
+                $this->mergeConfig($config, 'database.connections.'.$conn, [
+                    'database' => env('DB_DATABASE', 'creasi_test'),
+                    'username' => env('DB_USERNAME', 'creasico'),
+                    'password' => env('DB_PASSWORD', 'secret'),
+                ]);
             }
         });
+    }
+
+    private function mergeConfig(Repository $config, string $key, array $value)
+    {
+        $config->set($key, array_merge($config->get($key, []), $value));
     }
 }
