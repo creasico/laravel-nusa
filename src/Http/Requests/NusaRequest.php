0022fd76fdf3fdad3f8fd1be63b52a8604e00b22
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-09-09 09:41:41 +0700

    chore(api): add ability to filter by resource' codes
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/Http/Requests/NusaRequest.php
+++ src/Http/Requests/NusaRequest.php
@@ -11,15 +11,24 @@ class NusaRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'codes' => ['nullable', 'array'],
+            'codes.*' => ['nullable', 'numeric'],
             'search' => ['nullable', 'string'],
+            'page' => ['nullable', 'numeric'],
+            'per-page' => ['nullable', 'numeric'],
         ];
     }
 
     public function apply(Model $model)
     {
-        return $model->newQuery()
+        $result = $model->newQuery()
             ->when($this->has('search'), function (Builder $query) {
                 $query->search($this->query('search'));
+            })
+            ->when($this->has('codes'), function (Builder $query) use ($model) {
+                $query->whereIn($model->getKeyName(), (array) $this->query('codes'));
             });
+
+        return $result->paginate($this->query('per-page'));
     }
 }
