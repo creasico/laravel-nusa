91cc632e528c184263ec8b8a555889fa282ce7c6
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-11-10 09:22:16 +0700

    chore(db): rearrange `database` directory structure due to 3ff0e25
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Creasi\Nusa\Models\District;
+use Creasi\Nusa\Models\Province;
+use Creasi\Nusa\Models\Village;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        if (Province::query()->count() > 0) {
+            return;
+        }
+
+        $path = \realpath(\dirname(__DIR__).'/..').'/resources/json';
+
+        foreach ($this->loadJson($path, 'provinces.json') as $province) {
+            /** @var Province */
+            $province = Province::query()->create($province);
+
+            $province->regencies()->createMany(
+                $this->loadJson($path, $province->code, 'regencies.json')
+            );
+
+            District::query()->insert(
+                $this->loadJson($path, $province->code, 'districts.json')
+            );
+
+            Village::query()->insert(
+                $this->loadJson($path, $province->code, 'villages.json')
+            );
+        }
+    }
+
+    private function loadJson(string ...$paths): array
+    {
+        $path = \implode(\DIRECTORY_SEPARATOR, $paths);
+
+        return \json_decode(file_get_contents($path), true);
+    }
+}
